18cc5c6b4cafc67876038b6ec7186cdbc4392dc7
sanli33 <149355717+sanli33@users.noreply.github.com>
2023-10-30 13:48:37 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -1,10 +1,10 @@
 module.exports = {
-  appid: "wx71941f6106ae1072",
-  appsecret: "b4b2ad8a8d4a94e403831886ac10c646",
-  template_id: "q14lTqsDyK0N-KcZUs8UKHOl3GWn2uErDk-8hbm2ouQ",
+  appid: "wx59930cfada99a566",
+  appsecret: "2698ff1354bcd537ecc80e6521b8dd15",
+  template_id: "7Qk998N0gfv5zV_FTzjD-rLV4XwGVagYjEzkfskYiRc",
   touser: "oonWJ5slWuS2OrJdgCLM3GmNdxVk",
 
   key: "201c93c2e2b34bdeecdbf9f5265eb84e",
-  city: "邢台",
-  first_date: "2022-11-10",
+  city: "郑州",
+  first_date: "2023-10-21",
 };
